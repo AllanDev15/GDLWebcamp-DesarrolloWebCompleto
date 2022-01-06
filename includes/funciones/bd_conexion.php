0a95cd8fea212995420f4d767bc679bb55978018
Allan_PC <Allan.FL.15@live.com>
2022-01-06 13:53:46 -0600

    update db connection settings

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,6 +1,5 @@
 <?php 
-    $con = new mysqli('localhost', 'root', 'fosa980515', 'gdlwebcamp');
+    $con = new mysqli('us-cdbr-east-05.cleardb.net', 'b00601d5c8c502', '7252e53d', 'heroku_1e157c917ea1b5f');
     if($con->connect_error) {
         echo $error -> $con->connect_error;
     }
-?>
